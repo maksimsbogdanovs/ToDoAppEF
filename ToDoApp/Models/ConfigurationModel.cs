7c6b6df05a59baf2ca91d59aca741fc22dc17c92
Maksims Bogdanovs <maksims.bogdanovs@gmail.com>
2022-05-17 20:28:18 +0200

    Static members

---
+++ ToDoApp/Models/ConfigurationModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDoApp
+{
+    public /*static*/ class ConfigurationModel
+    {
+        public static string AppName = "My Daily ToDo list";
+
+        public static DateTime aDate = DateTime.Now;
+
+        public static string TodaysDate()
+        {
+            return aDate.ToString("MMMM dd");
+        }
+    }
+}
